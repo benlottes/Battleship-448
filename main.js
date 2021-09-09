bd3fef52c49bad4be53a0a1cf1cb56eae3f34d92
Bolu Adubi <bolu.adubi@gmail.com>
2021-09-09 14:27:21 -0500

    added cell IDs for both grids

--- main.js
+++ main.js
@@ -1,4 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => { 
 	canvas = document.querySelector("#myCanvas")
 	console.log("Canvas and Context Loaded");
+
+	let count = 0;
+
+	/*iterate through each cell in gridLeft by row and number them*/
+    $(".gridLeft .cell").each(function(){
+        $(this).attr("id", count);
+        count++;
+
+		$(this).click(function(){
+			console.log($(this).attr("id"))
+		});
+	});
+	count = 0;
+	/*iterate through each cell in gridRight by row and number them*/
+	$(".gridRight .cell").each(function(){
+        $(this).attr("id", count);
+        count++;
+
+		$(this).click(function(){
+			console.log($(this).attr("id"))
+		});
+	});
 })
