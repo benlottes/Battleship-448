26b60d2e729f1ea412e7be52eb60a659f631f2cd
Jowaki Merani <jowakimerani@ku.edu>
2021-09-08 21:01:28 -0500

    Added the allSunk() function and eddited the isSunk function to return 0 or 1

--- ship.js
+++ ship.js
@@ -6,6 +6,7 @@ class ship{
 		this.team = team;
 		this.rowHead = rowHead;
 		this.colHead = colHead;
+		this.counter = 0;
 	}
 	getSize(){
 		return this.size;
@@ -16,15 +17,35 @@ class ship{
 	getHead(){
 		return [this.rowHead, this.colHead];
 	}
-	isSunk(){
+	
+	isSunk(){//edited the sunk function to return 1 or 0 but left the other function just in case
+		for(let i = 0; i < size; i++){
+			if(hits[i] == 0){
+				return 0;
+			}
+		}
+		return 1;
+	}
+	/*isSunk(){
 		for(let i = 0; i < size; i++){
 			if(hits[i] == 0){
 				return false;
 			}
 		}
 		return true;
-	}
+	}*/
 	registerHit(distance){
 		this.hits[hitLocation] = 1;
+	}
+	
+	allSunk(){
+		//declare a member variable "counter"
+		counter=counter+isSunk();
+		if (counter==numberOfBoats){
+			return true;//all ships sunk 
+		}
+		else{
+			return false;//some shipps still remain
+		}
 	}
 }
