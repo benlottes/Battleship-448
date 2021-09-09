d00ab11d53b9395c06daec2033ce3fc6855fb2a8
Ben Lottes <benclottes@gmail.com>
2021-09-09 11:17:33 -0500

    Fixed a few syntax errors

--- board.js
+++ board.js
@@ -2,7 +2,7 @@ class board{
 	constructor (){
 		this.row=9;
 		this.column=10;
-		//this.board=new Array(this.row).fill().map(() => (new Array(this.column).fill().map(() => 0)));
+		this.board=new Array(this.row).fill().map(() => (new Array(this.column).fill().map(() => 0)));
 	}	
 	attemptedShot(row,col){
 		if(board[row][col]==0){
@@ -21,30 +21,30 @@ class board{
 		return false;
 	}
 	placeShip(newShip, rowTail, colTail){
-		[rowHead, colHead] = newShip.getHead;
+		let [rowHead, colHead] = newShip.getHead();
 		if(rowHead-rowTail == 0 && colHead-colTail == 0){
 			this.board[rowHead][colHead] = newShip;
 		}
 		else if(rowHead-rowTail == 0){
 			if(colHead-colTail < 0){
-				for(let i = colHead; i < colTail; i++){
+				for(let i = colHead; i <= colTail; i++){
 					this.board[rowHead][i] = newShip;
 				}
 			}
 			else{
-				for(let i = colHead; i > colTail; i--){
+				for(let i = colHead; i >= colTail; i--){
 					this.board[rowHead][i] = newShip;
 				}
 			}
 		}
 		else if(colHead-colTail == 0){
 			if(rowHead-rowTail < 0){
-				for(let i = rowHead; i < rowTail; i--){
+				for(let i = rowHead; i <= rowTail; i--){
 					this.board[i][colHead] = newShip;
 				}
 			}
 			else{
-				for(let i = rowHead; i > rowTail; i++){
+				for(let i = rowHead; i >= rowTail; i++){
 					this.board[i][colHead] = newShip;
 				}
 			}
